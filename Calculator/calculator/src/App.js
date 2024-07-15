3df005a27b5fd4d1eed068c194adf407966a9eef
shivanilahamage <shivanivlahamage@gmail.com>
2024-07-15 01:02:22 -0700

    Calculator-React

---
+++ Calculator/calculator/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import Calculator from "./Component/Calculator";
+
+function App() {
+  return (
+    <>
+      <Calculator />
+    </>
+  );
+}
+
+export default App;
